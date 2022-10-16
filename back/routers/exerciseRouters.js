6d863ebac67737f3d78b9d009b68665b5b8a6c52
Andrey Makhnev <ray.makhnev@gmail.com>
2022-10-16 16:09:09 +0300

    add exerciseLog controller

--- back/routers/exerciseRouters.js
+++ back/routers/exerciseRouters.js
@@ -1,9 +1,11 @@
-import {addNewExercise} from "../controllers/exercise/exerciseController.js";
+import {addNewExercise} from "../controllers/exercise/mainController.js";
+import {createNewExerciseLog} from "../controllers/exercise/logController.js";
 import {protect} from "../middleware/authMiddleware.js"
 import express from "express";
 
 const router = express.Router()
 
 router.route("/").post(protect, addNewExercise)
+router.route("/log").post(protect, createNewExerciseLog)
 
 export default router
