414df9547962f563aeb458e0693233b0fc40677a
Andrey Makhnev <ray.makhnev@gmail.com>
2022-10-22 17:33:52 +0300

    add exercise completed log controller

--- back/controllers/exercise/log/updateController.js
+++ back/controllers/exercise/log/updateController.js
@@ -1,19 +1,17 @@
 import asyncHandler from 'express-async-handler'
 import ExerciseLog from "../../../models/exerciseLogModel.js";
 
-// @desc    Update exerciseLog
+// @desc    Update exercise log
 // @route   PUT /api/exercises/log
 // @access  Private
 export const updateExerciseLog = asyncHandler(async (req, res) => {
     const { logId, timeIndex, key, value } = req.body
-
     const currentLog = await ExerciseLog.findById(logId)
 
     if (!currentLog) {
         res.status(404)
         throw new Error("Данный лог не найден!")
     }
-
 
     let newTimes = currentLog.times
 
@@ -23,16 +21,28 @@ export const updateExerciseLog = asyncHandler(async (req, res) => {
     }
 
     newTimes[timeIndex][key] = value
-
     currentLog.times = newTimes
 
     const updateLog = await currentLog.save()
 
-    // const exerciseLog = await ExerciseLog.update({
-    //     user: req.user._id,
-    //     exercise: exerciseId,
-    //     times: timesArray,
-    // })
+    res.json(updateLog)
+})
+
+// @desc    Update exercise complete log
+// @route   PUT /api/exercises/log/complete
+// @access  Private
+export const updateCompleteExerciseLog = asyncHandler(async (req, res) => {
+    const { logId, completed } = req.body
+    const currentLog = await ExerciseLog.findById(logId)
+
+    if (!currentLog) {
+        res.status(404)
+        throw new Error("Данный лог не найден!")
+    }
+
+    currentLog.completed = completed
+
+    const updateLog = await currentLog.save()
 
     res.json(updateLog)
 })
--- back/routers/exerciseRouters.js
+++ back/routers/exerciseRouters.js
@@ -2,14 +2,13 @@ import express from "express";
 import {protect} from "../middleware/authMiddleware.js"
 import {addNewExercise} from "../controllers/exercise/mainController.js";
 import {getExerciseLog} from "../controllers/exercise/log/getController.js";
-import {createNewExerciseLog} from "../controllers/exercise/log/createController.js";
-import {updateExerciseLog} from "../controllers/exercise/log/updateController.js";
+import {updateCompleteExerciseLog, updateExerciseLog} from "../controllers/exercise/log/updateController.js";
 
 const router = express.Router()
 
 router.route("/").post(protect, addNewExercise)
-// router.route("/log").post(protect, createNewExerciseLog)
 router.route("/log").put(protect, updateExerciseLog)
+router.route("/log/completed").put(protect, updateCompleteExerciseLog)
 router.route("/log/:id").get(protect, getExerciseLog)
 
 export default router
